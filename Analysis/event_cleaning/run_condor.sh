320a9b0339c6b2a87067fdff318733259cafd92e
Sofia Stepanoff <sstepanoff3@login-buzzard.pace.gatech.edu>
2025-10-09 16:04:37 -0400

    updated the htcondor scripts for submission for each individual file to be submitted as a job.

--- Analysis/event_cleaning/run_condor.sh
+++ Analysis/event_cleaning/run_condor.sh
@@ -1,19 +1,69 @@
 #!/bin/bash
 # run_condor.sh
 
+source /opt/root/bin/thisroot.sh
+
+# Setup ExACT
+export EXACT_DIR=/exact
+export LD_LIBRARY_PATH=/exact/dict:/usr/lib/oracle/21/client64/lib:/usr/local/lib:$LD_LIBRARY_PATH
+
+
 DATE=$1
-echo "Running job for $DATE"
+FILENAME=$2
+echo "Running job for $DATE with file $FILENAME "
 echo "Directories"
 ls -lh
-pwd
-python3 /srv/DailyData2file.py -d "$DATE" -i 1 -ifile "/srv/" -ofile "/srv/statemessages$DATE.csv"
-echo "ran python script"
+
+# Create necessary directories if they don't exist
+
+mkdir -p DataAnalysis/MergedData/Output/$DATE/
+mkdir -p DataAnalysis/event_cleaning/ClusterCleaning/
+mkdir -p DataAnalysis/event_cleaning/Output/
+mkdir -p DataAnalysis/flasher_calibration/Output/
+mkdir -p DataAnalysis/event_cleaning/Output/
+
+mv $FILENAME DataAnalysis/MergedData/Output/$DATE/
+mv neighbors DataAnalysis/event_cleaning/ClusterCleaning/
+# mv EventInfo.h DataAnalysis/event_cleaning/ClusterCleaning/
+# mv EventInfoDict_rdict.pcm DataAnalysis/event_cleaning/ClusterCleaning/
+# mv LinkDef.h DataAnalysis/event_cleaning/ClusterCleaning/
+# mv EventCleaning DataAnalysis/event_cleaning/ClusterCleaning/
+mv ${DATE}_FlasherCalibration_Factor.root DataAnalysis/flasher_calibration/Output/
+
+echo "Running eventcleaning for $DATE and File $FILENAME"
+# ldd ./FileMerge
+
+./EventCleaning $DATE ~/ $FILENAME
+# # sleep 10
+# echo "Running Calibration for $DATE and File $FILENAME"
+# ./AddCalibData $DATE /srv/ Merged_$FILENAME
 ls -lh
+# cd DataAnalysis/event_cleaning/Output/
+# rm *.pdf
+# rm *.root
+# for f in *.root; do
+#     [ -e "$f" ] || continue  # skip if no .root files
+#     time=$(echo "$FILENAME" | grep -oP '\d{4}-\d{2}-\d{2}T\K\d{2}:\d{2}')
+#     echo "Time extracted: $time"
+#     newname="${f%.root}_$time.root"
+#     echo "Renaming: $f → $newname"
+#     mv "$f" "$newname"
+# done
 
-echo "Created output file:"
-ls -lh statemessages*.csv
+cd DataAnalysis/event_cleaning/Output/DataFiles/
+for f in *.root; do
+    [ -e "$f" ] || continue  # skip if no .root files
+    time=$(echo "$FILENAME" | grep -oP '\d{4}-\d{2}-\d{2}T\K\d{2}:\d{2}')
+    echo "Time extracted: $time"
+    newname="${f%.root}_$time.root"
+    echo "Renaming: $f → $newname"
+    mv "$f" "$newname"
+done
 
-
-FILE="statemessages${DATE}.csv"
-FULL_PATH=$(realpath "$FILE")
-echo "Full path to output file: $FULL_PATH"
+# time=$(echo "$FILENAME" | grep -oP '\d{4}-\d{2}-\d{2}T\K\d{2}:\d{2}')
+# echo "Time extracted: $time"
+# newname="${filename%.root}_$time.root"
+# mv "$filename" "$newname"
+ls -lh
+pwd
+cd
--- Analysis/event_cleaning/run_script.sh
+++ Analysis/event_cleaning/run_script.sh
@@ -2,5 +2,6 @@
 # run_script.sh
 
 DATE=$1
+FILENAME=$2
 echo "Running for $DATE"
-apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/rootandexact.sif /mnt/DataAnalysis/event_cleaning/ClusterCleaning/EventCleaning $DATE y
+apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/rootandexact.sif /mnt/DataAnalysis/event_cleaning/ClusterCleaning/EventCleaning $DATE y $FILENAME    
