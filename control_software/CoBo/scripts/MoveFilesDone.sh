a362ba31d33591dce32282e4e8adfa1a6df47bb9
Trinity <trinity@trinityCPU.lawn.gatech.edu>
2026-07-28 16:58:30 +0000

    added a wait clause to try and fix the move AsAD error see labarchives scripts 20260727

--- control_software/CoBo/scripts/MoveFilesDone.sh
+++ control_software/CoBo/scripts/MoveFilesDone.sh
@@ -7,5 +7,17 @@ TIMESTAMP=$(date +'%Y%m%d')
 SAVEDATA="/home/trinity/Documents/Data"
 DESTDIR="${SAVEDATA}/${TIMESTAMP}/RawDataUnmerged"
 
-sudo mv ${FILEDIR}/*AsAd0* ${DESTDIR}
+ASAD_TIMEOUT=15
+ASAD_INTERVAL=5
+ASAD_ELAPSED=0
+until sudo mv ${FILEDIR}/*AsAd0* ${DESTDIR} 2>/dev/null; do
+    if [ ${ASAD_ELAPSED} -ge ${ASAD_TIMEOUT} ]; then
+        echo "ERROR: AsAD move failed after ${ASAD_TIMEOUT} seconds" >&2
+        exit 1
+    fi
+    echo "AsAD move failed, retrying in ${ASAD_INTERVAL}s..."
+    sleep ${ASAD_INTERVAL}
+    ASAD_ELAPSED=$((ASAD_ELAPSED + ASAD_INTERVAL))
+done
+
 sudo mv ${TBDIR}/TB_data* ${DESTDIR}
